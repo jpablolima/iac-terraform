b0a866a52c4cb5b296f078f006e1ba87acfb2fd5
João Pablo <jp.pablolima@gmail.com>
2023-04-16 18:59:38 -0300

    configuração de outputs

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,28 +1,22 @@
 variable "name" {
     description = "Name instance EC2"
     type = string
-  
 }
 
 variable "region" {
     description = "AWS Region"
     type = string
-  
 }
 
 variable "instance_type" {
     description = "instance Type"
     type = string
-  
 }
 
 variable "owners" {
     description = "Images Owners Canonical"
     type = string
-  
 }
-
-
 
 variable "image" {
     description = "Images Ubuntu Focal-20.04-amd64-server"
@@ -32,10 +26,8 @@ variable "image" {
 variable "key_name" {
     description = "Key"
     type = string
-  
 }
 
-
 variable "servers" {
-    
+   
 }
