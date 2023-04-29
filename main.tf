4d2c5b397c8799c73145798282fa656d6cf74fca
João Pablo <jp.pablolima@gmail.com>
2023-04-29 18:45:27 -0300

    github actions terraform plan dev

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = var.aws_region
-  profile = var.aws_profile ? var.aws_cli_profile : null
+  profile = var.aws_profile ? var.aws_profile : null
   # profile    = var.aws_profile
 }
 
