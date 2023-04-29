02466946942d0c66202f1362cbb7922d3243c084
João Pablo <jp.pablolima@gmail.com>
2023-04-29 18:58:51 -0300

    github actions terraform plan dev

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 provider "aws" {
   region     = var.aws_region
-  profile = var.aws_profile 
+  # profile = var.aws_profile 
+  access_key = "${var.AWS_ACCESS_KEY_ID}"
+  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
 
 }
 
